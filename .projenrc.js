8d00848d03aa08cc0ed67f806d33046b6003fce4
Steve Lewis <lewisste@argodata.com>
2023-12-12 21:33:16 -0600

    Basic expressjs app.

--- .projenrc.js
+++ .projenrc.js
@@ -3,7 +3,7 @@ const project = new javascript.NodeProject({
   defaultReleaseBranch: "main",
   name: "app-running",
 
-  // deps: [],                /* Runtime dependencies of this module. */
+   deps: ["express"],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
